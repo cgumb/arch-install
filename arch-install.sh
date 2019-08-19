3f9be2d60f89f73ae26167bb4ce59e460c95763f
cgumb <chrisgumb@gmail.com>
2019-08-19 00:13:20 -0400

    added bootloader

--- arch-install.sh
+++ arch-install.sh
@@ -1,49 +1,64 @@
 #!/bin/sh
 # Partition (cfdisk) /w swap
-PART='a'
+DEVICE='a'
 
-ROOT="/dev/sd${PART}1"
-SWAP="/dev/sd${PART}2" # check connection
+ROOT="/dev/sd${DEVICE}1"
+SWAP="/dev/sd${DEVICE}2" 
+
+# check connection
 
 #ip link
 #ping archlinux.org
 # Set date/time
+echo "Setting Up Date/Time"
 timedatectl set-ntp true
 timedatectl status
 
+echo "Formatting"
 mkfs.ext4 "$ROOT"
 mkswap "$SWAP"
 swapon "$SWAP"
 mount "$ROOT" /mnt
 
 # Edit Mirrors
+echo "Editing Mirror List"
 grep -A1 "United States" /etc/pacman.d/mirrorlist | \
 grep -v "\-\-" | grep -v "^#" > ./mirrorlist
 mv ./mirrorlist /etc/pacman.d/mirrorlist
-# Install!
-pacstrap /mnt base
 
+# Installation
+echo "Installing Base Arch"
+pacstrap /mnt base
+echo "Installation Complete!\n"
+
+echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "chroot"
 arch-chroot /mnt
-# set timezone
-#ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-# generate /etc/adjtime
-#hwclock --systohc
-# localization
-# uncomment en_US.UTF-8 UTF-8 in /etc/locale.gen
-#sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
-#locale-gen
-#echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Locale
+echo "Setting Locale"
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+generate /etc/adjtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # Network
-# echo "core" > /etc/hostname
+echo "Setting Up Network"
+echo "core" > /etc/hostname
 # edit /etc/hosts to show:
 # 127.0.0.1	localhost
 # ::1		localhost
 # 127.0.1.1	myhostname.localdomain	myhostname
+echo "127.0.0.1\tlocalhost\n\
+		::1\tlocalhost\n\
+		127.0.1.1\${HOST}.localdomain\t${HOST}" >> /etc/hosts
 
-# create root password
-# passwd
+echo "Creating Root Password"
+passwd
 # systemctl enable dhcpcd
 # useradd -m user
 # passwd user
@@ -53,5 +68,6 @@ arch-chroot /mnt
 # visudo
 
 # Bootloader
-#grub-install /dev/sdX
-#grub-mkconfig -o /boot/grub/grub.cfg
+echo "Installing Bootloader"
+grub-install "/dev/sd${DEVICE}"
+grub-mkconfig -o /boot/grub/grub.cfg
